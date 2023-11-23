c860c73f72210933b743e44e6a4a0e7fa0ae66de
fanna <ibrahfanna@gmail.com>
2023-11-23 12:05:31 +0100

    features migration controller model

---
+++ server/database/migrations/2023_11_23_082411_create_user_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->unsignedBigInteger('answer_id');
+            $table->unsignedBigInteger('question_id');
+            $table->varchar('user_id');
+            $table->text('answers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_answers');
+    }
+}
